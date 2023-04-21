13aad262465c0f7b16e61a6d6368194d8804fb1e
JGIOitesm <a01653261@itesm.com>
2023-04-20 19:52:51 -0600

    Add files to repository

--- audio_filter.py
+++ audio_filter.py
@@ -1,46 +1,49 @@
-#Libraries
+# Libraries
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.io import wavfile
 from playsound import playsound
+from scipy.io import wavfile
+
 
 def audio_filter():
-    #Play audio
-    playsound('MB_Song.wav')
+    # Play audio
+    playsound("MB_Song.wav")
 
-    #Read audio file
-    sampFreq, sound = wavfile.read('MB_Song.wav')
+    # Read audio file
+    sampFreq, sound = wavfile.read("MB_Song.wav")
     print(sound.dtype, sampFreq)
 
     # Normalice audio to b beetwen -1 to 1
-    sound = sound /2.0**15
+    sound = sound / 2.0**15
 
     # Just one channel
-    sound = sound [:,0]
+    sound = sound[:, 0]
 
     # Measure in seconds
     length_in_s = sound.shape[0] / sampFreq
     print("Audio length", length_in_s)
 
     # Audio plot
-    plt.plot(sound[:],'r')
+    plt.plot(sound[:], "r")
     plt.xlabel("Sound signal")
     plt.tight_layout()
     plt.show()
 
     # Time vector
     time = np.arange(sound.shape[0]) / sound.shape[0] * length_in_s
-    plt.plot(time, sound[:],'r')
+    plt.plot(time, sound[:], "r")
     plt.xlabel("time, signal")
     plt.tight_layout()
     plt.show()
 
     # Add noise to the signal
-    yerr = (0.005 * np.sin(2*np.pi * 6000 * time) +
-            0.008 * np.sin(2*np.pi * 8000 * time) +
-            0.006 * np.sin(2*np.pi * 2500 * time))
+    yerr = (
+        0.005 * np.sin(2 * np.pi * 6000 * time)
+        + 0.008 * np.sin(2 * np.pi * 8000 * time)
+        + 0.006 * np.sin(2 * np.pi * 2500 * time)
+    )
     signal = sound + yerr
-    
+
     # Zoom
     plt.plot(time[6000:7000], signal[6000:7000])
     plt.xlabel("time, s")
@@ -48,7 +51,7 @@ def audio_filter():
 
     # Fourier Transform
     fft_spectrum = np.fft.rfft(signal)
-    freq = np.fft.rfftfreq(signal.size, d=1./sampFreq)
+    freq = np.fft.rfftfreq(signal.size, d=1.0 / sampFreq)
     print("Fourier spectrum", fft_spectrum)
     fft_spectrum_abs = np.abs(fft_spectrum)
 
@@ -59,10 +62,19 @@ def audio_filter():
     plt.show()
 
     # Filtering working on FFT domain
-    fft_spectrum[5900:6100] = 0.0
+    fft_spectrum[5900:6100] = 0
+    fft_spectrum[7900:8100] = 0
+    fft_spectrum[2400:2600] = 0
+
+    # Plot FFT
+    fft_spectrum_abs = np.abs(fft_spectrum)
+    plt.plot(freq, fft_spectrum_abs)
+    plt.xlabel("Frequency Hz")
+    plt.ylabel("Amplitude, units")
+    plt.show()
 
     noiseless_signal = np.fft.irfft(fft_spectrum)
-    #Audio plot
+    # Audio plot
     plt.plot(time, noiseless_signal)
     plt.xlabel("time, signal")
     plt.tight_layout()
@@ -70,8 +82,9 @@ def audio_filter():
 
     wavfile.write("Noisy Audio.wav", sampFreq, signal)
     wavfile.write("Noiseless Audio.wav", sampFreq, noiseless_signal)
-    playsound('Noisy Audio.wav')
-    playsound('Noiseless Audio.wav')
+    playsound("Noisy Audio.wav")
+    playsound("Noiseless Audio.wav")
+
 
 if __name__ == "__main__":
     audio_filter()
